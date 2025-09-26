588d15d4c19224ca4f766de827f555de3ee2e242
lostmart <martinpedraza1979@gmail.com>
2025-09-26 15:22:59 +0200

    new siuntances

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import styles from "./App.module.scss"
+
+import bg from "./assets/bg.png"
+import house from "./assets/06.png"
+
 const App = () => {
-	return <div>Thuis is my app !</div>
+	return (
+		<main className={styles.main}>
+			<img className={styles.backgroundImage} src={bg} alt="background" />
+			<img src={house} alt="" />
+		</main>
+	)
 }
 
 export default App
