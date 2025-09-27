587154d322b2d4be10efa6e4e760e241e4af401a
lostmart <martinpedraza1979@gmail.com>
2025-09-27 10:46:55 +0200

    new faces

--- src/components/AboutComp.tsx
+++ src/components/AboutComp.tsx
@@ -1,7 +1,11 @@
 import styles from "./AboutComp.module.scss"
 
 const AboutComp = () => {
-	return <section className={styles.about}></section>
+	return (
+		<section className={styles.about}>
+			<h2>Some Medieval</h2>
+		</section>
+	)
 }
 
 export default AboutComp
