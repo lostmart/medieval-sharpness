c641ca8c53ea4e0d15c4ce80958c5b5156920f58
lostmart <lostmart@hotmail.com>
2025-09-28 13:48:09 +0200

    ne framer

--- src/components/AboutComp.tsx
+++ src/components/AboutComp.tsx
@@ -1,9 +1,18 @@
 import styles from "./AboutComp.module.scss"
+import { motion } from "framer-motion"
 
 const AboutComp = () => {
 	return (
 		<section className={styles.about}>
 			<h2>Some Medieval</h2>
+
+			<motion.div
+				initial={{ opacity: 0, y: 50 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+			>
+				Content appears on scroll
+			</motion.div>
 		</section>
 	)
 }
