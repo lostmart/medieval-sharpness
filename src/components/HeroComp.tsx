a9de88631c6abd9e5da0e0157871c12f9a82a45d
lostmart <lostmart@hotmail.com>
2025-10-07 09:51:58 +0200

    new instances

--- src/components/HeroComp.tsx
+++ src/components/HeroComp.tsx
@@ -17,12 +17,6 @@ const HeroComp = () => {
 
 	console.log(scrollY.get())
 
-	// Fast movement - small input range
-	// const fastElement = useTransform(scrollY, [0, 500], [0, 400])
-
-	// // Medium movement - medium input range
-	// const mediumElement = useTransform(scrollY, [0, 1000], [0, 400])
-
 	// Super slow - very large input range
 	const superSlowElement = useTransform(scrollY, [0, 5000], [0, -300])
 
@@ -43,39 +37,6 @@ const HeroComp = () => {
 
 	// front - very large input range
 	const fronterElement = useTransform(scrollY, [0, 3500], [0, -3500])
-
-	// const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
-
-	// useEffect(() => {
-	// 	const centerX = window.innerWidth / 2
-	// 	const centerY = window.innerHeight / 2
-
-	// 	const handleMouseMove = (event: MouseEvent) => {
-	// 		const relativeX = event.clientX - centerX
-	// 		const relativeY = event.clientY - centerY
-
-	// 		setMousePosition({
-	// 			x: relativeX,
-	// 			y: relativeY,
-	// 		})
-	// 	}
-
-	// 	window.addEventListener("mousemove", handleMouseMove)
-
-	// 	return () => {
-	// 		window.removeEventListener("mousemove", handleMouseMove)
-	// 	}
-	// }, [])
-
-	// const springStyle = {
-	// 	transition: "transform 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94)",
-	// 	backfaceVisibility: "hidden" as const,
-	// }
-
-	// const strongSpringStyle = {
-	// 	transition: "transform 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-	// 	backfaceVisibility: "hidden" as const,
-	// }
 
 	return (
 		<section className={styles.scene}>
